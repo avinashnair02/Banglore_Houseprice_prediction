2fddf279563ae4426dea6bdc5bb82e126fc079e2
Avinash Nair <48132861+avinashnair02@users.noreply.github.com>
2021-02-23 20:44:20 +0530

    Update server.py

--- server.py
+++ server.py
@@ -30,5 +30,5 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
