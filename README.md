5d36bfb9c1ecc0bda53ae4b853f4d5ad4b2c8aee
Avinash Nair <avinashnair02@gmail.com>
2021-04-15 13:05:11 +0530

    Updated somefils

---
+++ README.md
@@ -0,0 +1,29 @@
+![](UI.png)
+  
+  Banglore_Houseprice_prediction
+Used a Banglore house dataset to predict the price of the houses in the various regions in Banglore
+
+
+This is a complete project to build a real estate price prediction website using flask API.
+Built a model using sklearn and linear regression using the data from kaggle.com.
+Run the model file on the flask server.
+Building an interactive website using HTML, CSS, javascript that allows users to enter the values like bhk, number of bathrooms, and sqft.
+ 
+Technology and tool used for the project:
+
+
+1.Numpy and Pandas for data cleaning
+2.Matplotlib for data visualization
+3.Sklearn for model building
+4.Jupyter notebook, visual studio code, and spyder as IDE
+5Python flask for HTTP server
+6.Used postman to see the get and the post command
+7.HTML/CSS/Javascript for UI
+
+
+
+Working HEROKU lINk:https://house-pricepred-api.herokuapp.com/
+
+
+![](postman.PNG)
+
---
+++ util.py
@@ -0,0 +1,67 @@
+import pickle
+import json
+import numpy as np
+
+__locations = None
+__data_columns = []
+__model = None
+def get_round(x):
+    with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+        __model1 = pickle.load(f)
+    return round(__model1.predict([x])[0],2)
+
+
+              
+    
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        with open("./artifacts/columns.json", "r") as f:
+            __data_c = json.load(f)['data_columns']
+            print("HEllo ", __data_c)
+            loc_index = __data_c.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_c))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+
+
+    return get_round(x)
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global  __data_columns
+    global __locations
+
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
+
+    global __model
+    if __model is None:
+        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+            __model = pickle.load(f)
+    print("loading saved artifacts...done")
+
+def get_location_names():
+    with open("./artifacts/columns.json", "r") as f:
+        __data_c = json.load(f)['data_columns']
+        print("HEllo ", __data_c)
+    return __data_c[3:]
+
+def get_data_columns():
+    return __data_columns
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
